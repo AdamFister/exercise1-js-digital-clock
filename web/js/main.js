3f4a89da963d802fbab5c82a13fe84b6f7551371
Adam Fister <arfister@gmail.com>
2019-03-18 16:05:14 -0400

    Vue implementation complete

--- web/js/main.js
+++ web/js/main.js
@@ -1,96 +1,97 @@
-function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i;
+Vue.component('date', {
+    computed: {
+        formatDate() {
+            var today = new Date();
+            var dd = today.getDate();
+            var yyyy = today.getFullYear();
+
+            var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+            return currDate = (day[today.getDay()] + ' ' + month[today.getMonth()] + ' ' + dd + ', ' + yyyy);
+        }
+    },
+    template: `<h1>{{ formatDate }}</h1>`
+})
+
+var clock = new Vue({
+    el: '#app',
+    data: {
+        currentTime: new Date(),
+        interval: null,
+        time_str: "",
+        standardTime: "",
+        isMilitary: true
+    },
+    methods: {
+        displayTime: function () {
+            this.interval = setInterval(() => {
+                this.currentTime = new Date();
+
+                if (this.isMilitary) {
+                    this.time_str = this.getTime;
+                }
+                else {
+                    this.time_str = this.getStandardTime;
+                }
+                }, 1000)
+
+        }
+    },
+    beforeDestroy() {
+        clearInterval(this.interval);
+    },
+    created() {
+        this.displayTime();
+    },
+    computed: {
+        hrs() {
+            if (this.currentTime.getHours() < 10) {
+                return '0' + this.currentTime.getHours()
+            }
+            else return this.currentTime.getHours()
+        },
+        meridiem() {
+            if (this.currentTime.getHours() >= 12) {
+                return "PM";
+            }
+            else if (this.currentTime.getHours() >= 0 && this.currentTime.getHours() < 12) {
+                return "AM";
+
+            }
+
+        },
+        getStandardTime() {
+            return this.standardHours + ":" + this.mins + ":" + this.sec + " " + this.meridiem
+        },
+        mins() {
+            if (this.currentTime.getMinutes() < 10) {
+                return '0' + this.currentTime.getMinutes()
+            }
+            else return this.currentTime.getMinutes()
+        },
+        sec() {
+            if (this.currentTime.getSeconds() < 10) {
+                return '0' + this.currentTime.getSeconds()
+            }
+            else return this.currentTime.getSeconds()
+        },
+        getTime() {
+            return this.hrs + ":" + this.mins + ":" + this.sec
+        },
+        standardHours() {
+            if (this.currentTime.getHours() > 12) {
+                return this.currentTime.getHours() - 12;
+            }
+            else if (this.currentTime.getHours() == 0) {
+                return 12;
+            }
+            else {
+                return this.currentTime.getHours();
+            }
+        }
     }
-    return i;
-}
-
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    // add a zero in front of numbers<10
-    m = checkTime(m);
-    s = checkTime(s);
-
-    // JDH Edit
-    updatePage('time', h + ":" + m + ":" + s, "1000");
-    
-    t = setTimeout(function () {
-        startTime()
-    }, 500);
-}
-
-var dateObj = new Date();
-var month = dateObj.getUTCMonth() + 1; //months from 1-12
-var day = dateObj.getUTCDate();
-var year = dateObj.getUTCFullYear();
-
-if (day < 10) {
-    day = '0' + day;
-}
-
-if (month < 10) {
-    month = '0' + month;
-}
-
-// JDH Edit
-    updatePage('date', month + "/" + day + "/" + year, "800");
 
 
 
-function startTime2() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-
-    if (h > 12) {
-        h -= 12;
-    } else if (h === 0) {
-        h = 12;
-    }
-    // add a zero in front of numbers<10
-    m = checkTime(m);
-    s = checkTime(s);
-    
-    // JDH Edit
-    updatePage('time2', h + ":" + m + ":" + s, "1000");
-    
-    t = setTimeout(function () {
-        startTime2()
-    }, 500);
-}
-startTime2();
-
-function myFunction() {
-    var mainFrameOne = document.getElementById("mainFrameOne"); 
-    var mainFrameTwo = document.getElementById("mainFrameTwo");
- 
-    mainFrameOne.style.display = (
-        mainFrameOne.style.display == "none" ? "block" : "none"); 
-    mainFrameTwo.style.display = (
-        mainFrameTwo.style.display == "none" ? "block" : "none"); 
- }
-
-// function hideButton() {
-//     var x = document.getElementById("time");
-//     if (x.style.display === "none") {
-//       x.style.display = "block";
-//     } else {
-//       x.style.display = "none";
-//     }
-//   }
-
-function updatePage (elementID, timeStr, fontSize){
-    //elementID is the element you want to change
-    //timeStr is the concatenation of the time values
-    //fontSize is the string value of the percentage to change
-    //note: didn't see a place for you to change the fontFamily after the page loaded
-    document.getElementById(elementID).innerHTML = timeStr;
-    document.getElementById(elementID).style.fontFamily = "monospace";
-    document.getElementById(elementID).style.fontSize = fontSize+"%";
-}
-
-startTime();
+})
